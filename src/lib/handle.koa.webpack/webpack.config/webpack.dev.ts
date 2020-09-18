4d31c96f6d7682661b7b93c09194c4a9713ffdeb
profore <910058244@qq.com>
2020-09-18 14:41:09 +0800

    优化webpack配置

--- src/lib/handle.koa.webpack/webpack.config/webpack.dev.ts
+++ src/lib/handle.koa.webpack/webpack.config/webpack.dev.ts
@@ -10,10 +10,5 @@ export default merge(common, {
     poll: 1000, // 监测修改的时间(ms)
     aggregateTimeout: 500, // 防止重复按键，500毫秒内算按键一次
     ignored: /node_modules/ // 不监测
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      WEBPACK_MODE: JSON.stringify('development')
-    })
-  ]
+  }
 })
--- src/lib/handle.koa.webpack/webpack.config/webpack.prod.ts
+++ src/lib/handle.koa.webpack/webpack.config/webpack.prod.ts
@@ -1,14 +1,10 @@
 import merge from 'webpack-merge'
 import common from './webpack.common'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
-import webpack from 'webpack'
 
 export default merge(common, {
   mode: 'production',
   plugins: [
-    new CleanWebpackPlugin(),
-    new webpack.DefinePlugin({
-      WEBPACK_MODE: JSON.stringify('production')
-    })
+    new CleanWebpackPlugin()
   ]
 })
